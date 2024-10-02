201b49ea04aa2a0923bf3cb88bf83eeef793d8ad
Ryan_Vesta <knowhoweugene@gmail.com>
2024-10-01 21:44:43 -0700

    SmOutcome is completely built and tested!

--- lib/smoutcome.dart
+++ lib/smoutcome.dart
@@ -10,7 +10,7 @@ class SmOutcome {
   ///Maps Table UUID Strings to this Outcome's weight on the Table with the UUID key.
   Map<String, int> weights;
   ///Tag UUID strings.
-  List<String> tags;
+  Set<String> tags;
 
   SmOutcome({
     required this.id,
@@ -25,9 +25,10 @@ class SmOutcome {
           id: outcomeMap[kOUTCOME_ID] ?? const Uuid().v1(),
           rawText: outcomeMap[kOUTCOME_RAWTEXT] ?? "",
           weights: outcomeMap[kOUTCOME_WEIGHTS] ?? {},
-          tags: outcomeMap[kOUTCOME_TAGS] ?? [],
+          tags: outcomeMap[kOUTCOME_TAGS] ?? <String>{},
         );
-  ///Builds a map that fromMap can use to rebuild this SmOutcome
+
+  ///Builds a map that fromMap can use to rebuild this SmOutcome.
   Map<String, dynamic> toMap() {
     return {
       kOUTCOME_ID: id,
@@ -38,10 +39,38 @@ class SmOutcome {
   }
 
   ///Provides a blank SmOutcome with a valid UUID
-  SmOutcome.blank() : this( id: const Uuid().v1(), rawText: "", weights: {}, tags: []);
+  SmOutcome.blank() : this( id: const Uuid().v1(), rawText: "", weights: {}, tags: <String>{});
 
   ///Retreives specific weights as though the SmOutcome object is itself a map.
   int? operator[](String tableID) {
     return weights[tableID];
+  }
+
+  ///Adds new weights or updates existing weights as though the SmOutcome is itself a map.
+  void operator[]=(String tableID, int weight) {
+    weights[tableID] = weight;
+  }
+
+  ///Removes the specified Table UUID and its associated weight
+  void remove(String tableIDtoRemove) {
+    weights.remove(tableIDtoRemove);
+  }
+
+  ///Asserts if this Outcome is part of the provided Table
+  bool isOnTable(String tableID) {
+    return weights.containsKey(tableID);
+  }
+
+  void addTags(Iterable<String> newTags) {
+    tags.addAll(newTags);
+  }
+
+  void removeTags(Iterable<String> tagsToRemove) {
+    tags.removeAll(tagsToRemove);
+  }
+
+  ///Asserts if this Outcome has the provided Tag
+  bool isTagged(String targetTagID) {
+    return tags.contains(targetTagID);
   }
 }
--- test/sm_data_structs_test.dart
+++ test/sm_data_structs_test.dart
@@ -19,7 +19,7 @@ void main() {
     String table2ID = const Uuid().v1();
     int table2Weight = 1;
     String outcome1ID = const Uuid().v1();
-    List<String> outcome1tags = [const Uuid().v1(), const Uuid().v1()];
+    Set<String> outcome1tags = {const Uuid().v1(), const Uuid().v1()};
 
     Map<String, dynamic> mOutcomeComplete = {
       kOUTCOME_ID: outcome1ID,
@@ -67,12 +67,102 @@ void main() {
       //complete map
       expect(outcome1.tags, outcome1tags);
       //incomplete map
-      expect(outcome2.tags, []);
+      expect(outcome2.tags, <String>{});
     });
   });
 
   group('SmOutcome methods', (){
-    test('toMap', (){});
+    String mRawText = "mock Outcome raw text";
+    String table1ID = const Uuid().v1();
+    int table1Weight = 1;
+    String table2ID = const Uuid().v1();
+    int table2Weight = 1;
+    String outcome1ID = const Uuid().v1();
+    String outcome1tag1 = const Uuid().v1();
+    String outcome1tag2 = const Uuid().v1();
+    Set<String> outcome1tags = {outcome1tag1, outcome1tag2};
+    
+    Map<String, dynamic> mOutcomeComplete = {
+      kOUTCOME_ID: outcome1ID,
+      kOUTCOME_RAWTEXT: mRawText,
+      kOUTCOME_WEIGHTS: {table1ID: table1Weight, table2ID: table2Weight},
+      kOUTCOME_TAGS: outcome1tags,
+    };
+    
+    test('toMap', (){
+      final outcome = SmOutcome.fromMap(mOutcomeComplete);
+
+      expect(outcome.toMap(), mOutcomeComplete);
+    });
+
+    test('Add table weight', (){
+      final outcome = SmOutcome.fromMap(mOutcomeComplete);
+      final String newTableID = const Uuid().v1();
+      const int newTableWeight = 3;
+
+      outcome[newTableID] = newTableWeight;
+
+      expect(outcome[newTableID], newTableWeight);
+    });
+
+    test('Remove table weight', (){
+      final SmOutcome outcome = SmOutcome.fromMap(mOutcomeComplete);
+      outcome.remove(table1ID);
+
+      expect(outcome[table1ID], null);
+    });
+
+    test('Update table weight', (){
+      final SmOutcome outcome = SmOutcome.fromMap(mOutcomeComplete);
+      int newWeight = table1Weight + 10;
+      outcome[table1ID] = newWeight;
+
+      expect(outcome[table1ID], newWeight);
+    });
+
+    test('Is on table?', (){
+      final SmOutcome outcome = SmOutcome.fromMap(mOutcomeComplete);
+      final String otherTable = Uuid().v1();
+
+      //Outcome IS on the table
+      expect(outcome.isOnTable(table1ID), true);
+      //Outcome IS NOT on the table
+      expect(outcome.isOnTable(otherTable), false);
+    });
+
+    test('Add tags', (){
+      final SmOutcome outcome = SmOutcome.fromMap(mOutcomeComplete);
+      final List<String> newTags = [Uuid().v1(), Uuid().v1()];
+      
+      Set<String> expectedSet = {};
+      expectedSet.addAll(outcome.tags);
+      expectedSet.addAll(newTags);
+      
+      outcome.addTags(newTags);
+
+      expect(outcome.tags, expectedSet);
+    });
+
+    test("Remove tags", (){
+      final SmOutcome outcome = SmOutcome.fromMap(mOutcomeComplete);
+      final List<String> newTags = [Uuid().v1(), Uuid().v1()];
+
+      Set<String> removeTheseTags = outcome1tags.toSet();
+      outcome.addTags(newTags);
+      outcome.removeTags(removeTheseTags);
+
+      expect(outcome.tags, newTags);
+    });
+  
+    test("Is tagged", (){
+      final SmOutcome outcome = SmOutcome.fromMap(mOutcomeComplete);
+      outcome.addTags({outcome1tag1});
+      
+      //IS tagged
+      expect(outcome.isTagged(outcome1tag1), true);
+      //is NOT tagged
+      expect(outcome.isTagged(const Uuid().v1()), false);
+    });
   });
   
    group('OutcomeManager', () {
